f64b709f6e47c6f49cd873996da335cdbc8124ec
Simon Opheim <simon.opheim@gmail.com>
2020-08-22 19:02:09 +0200

    feat: add styling to login form

--- src/components/LoginForm.svelte
+++ src/components/LoginForm.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { login as loginAuth } from '../auth';
+  import Auth from '../auth';
 
   let loggingIn = false;
   let error = '';
@@ -10,7 +10,7 @@
     e.preventDefault();
     loggingIn = true;
     try {
-      await loginAuth(email, password);
+      await Auth.login(email, password);
     } catch (err) {
       error = err.message;
     } finally {
@@ -21,21 +21,40 @@
 
 <style>
   .login-form {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    grid-template-rows: 1fr 1fr;
-    gap: 2px;
-    position: absolute;
-    bottom: 10vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    height: 100px;
+    width: 280px;
   }
 
   .login-form__field {
-    grid-column: span 2;
+    font-size: 1.4em;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .login-form__field label {
+    font-size: 1.6em;
+  }
+
+  .login-form__field input {
+    padding-left: 10px;
   }
 
   .login-form__submit-btn {
-    grid-column: 2 / 4;
-    grid-row: 2;
+    font-size: 1.6em;
+    background-color: lightgreen;
+    margin: 1em;
+    padding: 0.5em;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+    border: none;
+  }
+
+  .login-form__submit-btn:hover {
+    cursor: pointer;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.4);
   }
 </style>
 
@@ -45,14 +64,14 @@
   {:else if error}
     <p>error</p>
   {:else}
-    <label class="login-form__field">
-      Epost:
-      <input type="email" bind:value={email} />
-    </label>
-    <label class="login-form__field">
-      Passord:
-      <input type="password" bind:value={password} />
-    </label>
+    <div class="login-form__field login-form__field--email">
+      <label for="email">Epost:</label>
+      <input id="email" type="email" bind:value={email} />
+    </div>
+    <div class="login-form__field login-form__field--password">
+      <label for="password">Passord:</label>
+      <input id="password" type="password" bind:value={password} />
+    </div>
     <button type="submit" class="login-form__submit-btn">Logg inn</button>
   {/if}
 </form>
