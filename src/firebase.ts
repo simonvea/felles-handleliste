c7fb4fdb08194e10703f294404a93cc6286bf375
Simon Opheim <simon.opheim@gmail.com>
2020-09-19 14:03:47 +0200

    fix: remove config from git

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,6 @@
 import firebase from 'firebase/app';
+import config from '../firebaseConfig';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyB_FyKIbpRVTyjw4JrcPb8bbvVmTB7SBvI',
-  authDomain: 'felles-handleliste.firebaseapp.com',
-  databaseURL: 'https://felles-handleliste.firebaseio.com',
-  projectId: 'felles-handleliste',
-  storageBucket: 'felles-handleliste.appspot.com',
-  messagingSenderId: '925360885806',
-  appId: '1:925360885806:web:82d4b30bbb549ed2828fbf',
-};
-
-export const app = firebase.initializeApp(firebaseConfig);
+export const app = firebase.initializeApp(config);
