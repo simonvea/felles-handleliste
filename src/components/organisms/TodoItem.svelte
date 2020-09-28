e706331f1c2e990efda08af7e0b974f6939e2f60
Simon Opheim <simon.opheim@gmail.com>
2020-09-28 20:24:04 +0200

    WIP: start work on drag 'n drop

--- src/components/organisms/TodoItem.svelte
+++ src/components/organisms/TodoItem.svelte
@@ -5,6 +5,7 @@
   import type { Todo } from '../../types';
   import CheckBox from '../atoms/CheckBox.svelte';
   import TodoInput from '../atoms/TodoInput.svelte';
+  import AddTodo from './AddTodo.svelte';
 
   export let todo: Todo;
 
@@ -29,14 +30,54 @@
     const todo = $todos.find((todo) => todo.id === id);
     API.update({ ...todo, done: !prevDone });
   }
+
+  function drag(e: DragEvent, id: string) {
+    e.dataTransfer.setData('text', id);
+    console.log('drar', id);
+  }
+
+  function onDragover(e: DragEvent) {
+    e.preventDefault();
+    const element = e.target;
+    (element as HTMLElement).classList.add('dragover');
+  }
+
+  function onDragLeave(e) {
+    e.target.classList.remove('dragover');
+  }
+
+  function onDrop(e: DragEvent) {
+    e.preventDefault();
+    const id = e.dataTransfer.getData('text');
+    console.log('motok id', id);
+  }
 </script>
 
-<article class="flex items-center">
-  <CheckBox
-    checked={todo.done}
-    onClick={() => toggleDone(todo.id, todo.done)} />
-  <TodoInput
-    value={todo.task}
-    onInput={({ target }) => updateTodoAsync(todo.id, target.value)} />
-  <DeleteButton onClick={() => deleteTodo(todo.id)} />
+<style>
+  .dragover {
+    height: 5rem;
+    border: lightgray dashed 1px;
+  }
+</style>
+
+<article class="flex items-center flex-col">
+  <div
+    class="h-1 w-full dragover"
+    on:dragover={onDragover}
+    on:drop={onDrop}
+    on:dragleave={onDragLeave} />
+  <div
+    class="flex items-center"
+    draggable="true"
+    on:dragstart={(e) => drag(e, todo.id)}
+    on:dragover={(e) => e.preventDefault()}>
+    <CheckBox
+      checked={todo.done}
+      onClick={() => toggleDone(todo.id, todo.done)}
+      on:dragover={(e) => e.preventDefault()} />
+    <TodoInput
+      value={todo.task}
+      onInput={({ target }) => updateTodoAsync(todo.id, target.value)} />
+    <DeleteButton onClick={() => deleteTodo(todo.id)} />
+  </div>
 </article>
