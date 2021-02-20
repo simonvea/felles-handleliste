e04179ee754d1a7194841bf213871cea78c7b5a0
Simon Opheim <simon.opheim@gmail.com>
2021-02-20 16:33:08 +0100

    fix: display a message if no todosToShow

--- src/views/Todos.svelte
+++ src/views/Todos.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import TodoList from '../components/ui/TodoList.svelte';
   import AddTodo from '../components/organisms/AddTodo.svelte';
-  import { todos, remainingTodos } from '../store/Todos';
+  import { todosToShow, remainingTodos } from '../store/Todos';
 </script>
 
 <section class="relative max-w-md">
   <section class="text-center m-4">
     <p>{$remainingTodos.length} gjenstår.</p>
   </section>
-  {#if $todos.length > 0}
+  {#if $todosToShow.length > 0}
     <section class="mb-24">
       <TodoList />
     </section>
